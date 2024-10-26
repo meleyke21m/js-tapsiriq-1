87dd8964c59c03bf987c01b6719f368a4014f082
meleyke21m <melimahmudlu7@gmail.com>
2024-10-26 19:40:42 +0400

    js tapsiriq1 addd
    js tapsiriq 1 addd

---
+++ js/main.js
@@ -0,0 +1,107 @@
+//console.log('hello');
+// alert("sehifeye xos gelmisiniz")
+// prompt("adinizi daxil edin")
+// confirm("yasinizi daxil edin")
+// document.write("hgfbnjuy")
+// document.writeln("nhgfr")
+
+// let user =prompt(" reqem daxil edin")
+
+// user > 0 ? console.log("musbetdir") : console.log("menfidir");
+  
+// let number = 0;
+// while(number <100){
+//     console.log(number);
+//     if(number == 50){
+//        break
+
+//     }
+//     number++
+// }
+
+let username = 10
+switch(username){
+    case 10: console.log("cavab 10 dur")
+    break;
+    case 7: console.log("cavab 7dir")
+}
+
+let number =5
+if(number % 2===0){
+    console.log(" cutdur")
+}else{
+    console.log("  tekdir")
+}
+
+
+let number1 =prompt("reqem daxil edin")
+
+if(number1>0){
+    console.log("musbetdir")
+}else{
+    console.log("menfidir")
+}
+
+var number3 = 4;
+let number2 = 4;
+console.log(number3 == number2)
+
+var number5=10
+let number6="20"
+console.log(number5 === number6)
+
+let name1= "meli"
+let name2= "meli"
+
+if(name1 && name2){
+    console.log("adlar bir-birine beraberdir ")
+}else{
+    console.log(" adlar bir-birine beraber deyil")
+}
+
+let name3= 10<5
+let name4= 5>10
+
+if(name3 || name4){
+    console.log("adlar bir-birine beraberdir ")
+}else{
+    console.log(" adlar bir-birine beraber deyil !")
+}
+
+
+let username2= true
+console.log(!username);
+
+let name8="meli"
+let name9="meleyke"
+
+
+if(name8 != name9){
+    console.log("adlar bir-birine beraber deyilll !")
+}
+
+let number4= 9
+if(number4 >5 && number4 <10 ){
+
+    console.log(" true ");
+
+}else{
+    console.log("false");
+}
+    
+
+
+let number8 = prompt (" reqem daxil edin")
+
+if(number8 > 0){
+
+    console.log("musbet");
+
+}else if(number8==0){
+
+    console.log("cavab 0");
+}else{
+
+    console.log("menfidir");
+}
+
